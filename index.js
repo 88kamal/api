cad47768497c12eb6e7c3752249e661406590eac
KAMAL NAYAN UPADHYAY <knupadhyay784@gmail.com>
2023-07-27 10:39:25 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const cors = require("cors")
+const app = express()
+app.use(cors());
+
+const port = process.env.PORT || 2000 // loaclhost port and heroku port 
+
+const apiData = require("./data.json") // api data import 
+
+
+app.get("/", (req,res)=>{
+    res.send(apiData); // get apiData
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})

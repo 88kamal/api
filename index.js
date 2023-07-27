225a9310fa92079cde74b641568ed68ef963a8af
KAMAL NAYAN UPADHYAY <knupadhyay784@gmail.com>
2023-07-27 10:47:12 +0530

    done

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const port = process.env.PORT || 2000 // loaclhost port and heroku port
 
 const apiData = require("./data.json") // api data import 
 
-app.get("/service", (req,res)=>{
+app.get("/ ", (req,res)=>{
     res.send(apiData); // get apiData
 })
 
